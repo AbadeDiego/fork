7b9cd9614c801f80d78e7d0d5081655db25acb7e
Diego Freire <freirediego128@gmail.com>
2023-06-20 15:11:38 -0300

    updates

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-…or create a new repository on the command line
+### …or create a new repository on the command line
 
-echo "# 4211" >> README.md
-git init
-git add README.md
-git commit -m "third commit"
-git branch -M main
-git remote add origin https://github.com/AbadeDiego/blog.git
-git push -u origin main
+- echo "# 4211" >> README.md
+- git init
+- git add README.md
+- git commit -m "third commit"
+- git branch -M main
+- git remote add origin https://github.com/AbadeDiego/blog.git
+- git push -u origin main
 
-…or push an existing repository from the command line
+### …or push an existing repository from the command line
 
-git remote add origin https://github.com/AbadeDiego/4211.git
-git branch -M main
-git push -u origin main
+- git remote add origin https://github.com/AbadeDiego/blog.git
+- git branch -M main
+- git push -u origin main
--- user/static/js/header.js
+++ user/static/js/header.js
@@ -1,14 +1,78 @@
+// --------------- Navbar Menu ----------------- //
+
 var menuList = document.getElementById("menuList");
 
-menuList.style.maxHeight = "385px";
+menuList.style.maxHeight = "400px";
 
 function togglemenu(){
   if(menuList.style.maxHeight == "0px")
   {
-    menuList.style.maxHeight = "385px";
+    menuList.style.maxHeight = "400px";
   }
   else
   {
     menuList.style.maxHeight = "0px";
   }
 }
+
+// --------------- Comentários Criadores  ----------------- //
+
+var currentIndex = 0;
+var divs = document.querySelectorAll(".transition-div");
+
+function showDivs() {
+  for (var i = 0; i < divs.length; i++) {
+    if (i === currentIndex || i === currentIndex + 1) {
+      divs[i].classList.add("show");
+    } else {
+      divs[i].classList.remove("show");
+    }
+  }
+}
+
+function prevDiv() {
+  if (currentIndex > 0) {
+    currentIndex -= 1;
+    showDivs();
+  }
+}
+
+function nextDiv() {
+  if (currentIndex < divs.length - 1) {
+    currentIndex += 1
+    showDivs();
+  }
+}
+
+showDivs();
+
+// --------------- Funcionalidades BodeTech  ----------------- //
+
+var currentIndexCard = 0;
+var cards = document.querySelectorAll(".transition-card");
+
+function showCards() {
+  for (var i = 0; i < cards.length; i++) {
+    if (i === currentIndexCard || i === currentIndexCard + 1) {
+      cards[i].classList.add("show");
+    } else {
+      cards[i].classList.remove("show");
+    }
+  }
+}
+
+function prevCard() {
+  if (currentIndexCard > 0) {
+    currentIndexCard -= 1;
+    showCards();
+  }
+}
+
+function nextCard() {
+  if (currentIndexCard < cards.length - 1) {
+    currentIndexCard += 1;
+    showCards();
+  }
+}
+
+showCards();
