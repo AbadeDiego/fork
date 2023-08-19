6c41a6e1c190f499ea0c2dd062687d706db764e6
semdesperdicio <semdesperdicio1@gmail.com>
2023-08-19 16:33:25 -0300

    third commit

--- user/views.py
+++ user/views.py
@@ -13,6 +13,15 @@ def index(request):
         if email:
             Subscription.objects.create(email=email)
             success_message = 'Obrigado por se cadastrar!'  # Definir a mensagem de sucesso
+
+            # Enviar notificação por email
+            send_mail(
+                'Nova inscrição na newsletter',
+                f'Um novo usuário se inscreveu na newsletter.\nEmail: {email}',
+                'semdesperdicio1@gmail.com',  # Substitua pelo seu endereço de email
+                ['semdesperdicio1@gmail.com'],  # Substitua pelo email para receber notificações
+                fail_silently=False,
+            )
     return render(request, 'user/index.html', {'title': 'home', 'success_message': success_message})
 #################### Envio de Mensagem #######################################
 
@@ -26,10 +35,10 @@ def enviar_mensagem(request):
         message = request.POST.get('message')
 
         # Envie o email
-        subject = 'Nova mensagem de contato'
+        subject = 'Nova mensagem de contato '
         message = f'Nome: {first_name} {last_name}\nEmail: {email}\nTelefone: {phone}\n\nMensagem: {message}'
         from_email = settings.DEFAULT_FROM_EMAIL
-        to_email = ['riseit.is@gmail.com']  # Substitua pelo seu endereço de email
+        to_email = ['semdesperdicio1@gmail.com']  # Substitua pelo seu endereço de email
 
         send_mail(subject, message, from_email, to_email, fail_silently=False)
 
